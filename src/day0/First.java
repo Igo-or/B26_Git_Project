55cea1baca4b0bbdbf1c5657de9723bf5ebb417f
igorgirnyk <ihor.girnyk@gmail.com>
2022-03-26 15:10:58 -0500

    Added some code

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,17 +2,14 @@ package day0;
 
 public class First {
 
-    //TODO: Works on this tomorrow
-    // add some cool code
-    // edit that cool code
-
-
     /* extra line for practice
     one more line
     and one more */
 
     public static void main(String[] args) {
         System.out.println("Java is Fun");
+
+        System.out.println("Working...");
     }
 
 }
