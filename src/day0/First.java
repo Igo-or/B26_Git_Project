b7d59dfe21a6c1b9348193625b293b90423d477e
igorgirnyk <ihor.girnyk@gmail.com>
2022-03-26 15:04:14 -0500

    Created First file with TODO list

---
+++ src/day0/First.java
@@ -0,0 +1,13 @@
+package day0;
+
+public class First {
+
+    //TODO: Works on this tomorrow
+    // add some cool code
+    // edit that cool code
+
+    public static void main(String[] args) {
+        System.out.println("Java is Fun");
+    }
+
+}
