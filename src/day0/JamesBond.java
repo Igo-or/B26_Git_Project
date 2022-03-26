bb620f1e4f8a87cf597a5d121fb85387055ffc8c
OviumPro <98862570+OviumPro@users.noreply.github.com>
2022-03-26 15:28:22 -0500

    Create JamesBond.java

---
+++ src/day0/JamesBond.java
@@ -0,0 +1,16 @@
+package day0;
+
+
+/* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Pellentesque aliquam massa id justo molestie elementum.
+Phasellus dictum est id nulla varius, a sollicitudin nibh semper.
+Vestibulum maximus turpis ut ipsum imperdiet, eget dignissim urna pellentesque.
+Sed semper turpis vitae nisl condimentum condimentum.
+Maecenas ac sem dignissim, viverra justo vitae, eleifend ligula.
+Proin at erat placerat, tempor justo ac, gravida est.
+Vivamus a sapien in lacus vehicula posuere. */
+
+public static void main(String [] args){
+ System.out.println("James BOND is COMING!"); 
+  }
+}
